fcdfa3bea44b350cd1db22f1bb48eb01f5c30493
RobinTheRedLight <robinmitra789@gmail.com>
2024-05-21 16:32:15 +0600

    product-validation creation completed

---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+const variantsSchema = z.object({
+  type: z.string().min(1, 'Type is required'),
+  value: z.string().min(1, 'Value is required'),
+});
+
+const inventorySchema = z.object({
+  quantity: z
+    .number()
+    .int()
+    .min(0, 'Quantity is required and the value should be in integer'),
+  inStock: z.boolean(),
+});
+
+const productSchema = z.object({
+  name: z.string().min(1, 'Name is required').trim(),
+  description: z.string().min(1, 'Description is required').trim(),
+  price: z
+    .number()
+    .positive('Price must be required and it should be in a positive number'),
+  category: z.string().min(1, 'Category is required'),
+  tags: z
+    .array(z.string().min(1, 'Tag cannot be empty'))
+    .min(1, 'Tags are required'),
+  variants: z.array(variantsSchema).min(1, 'Variants are required'),
+  inventory: inventorySchema,
+  isDeleted: z.boolean().optional(),
+});
+
+export default productSchema;
