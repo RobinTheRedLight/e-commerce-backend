c4f4f6bfa1478d3af0e0a249e5fe3d3ccf782943
RobinTheRedLight <robinmitra789@gmail.com>
2024-05-22 15:33:23 +0600

    order-update-section completed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import { ProductRoutes } from './app/modules/product/product.route';
+import { OrderRoutes } from './app/modules/order/order.route';
 
 const app: Application = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/products', ProductRoutes);
+app.use('/api/orders', OrderRoutes);
 
 const serverMessage = (req: Request, res: Response) => {
   res.send('Server is connected');
--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,9 +1,31 @@
+import { Product } from '../product/product.model';
 import { TOrder } from './order.interface';
 import { Order } from './order.model';
 
 const createOrderIntoDB = async (orderData: TOrder) => {
-  const result = await Order.create(orderData);
-  return result;
+  const id = orderData.productId.toString();
+  const orderQuantity = orderData.quantity;
+  const existData = await Product.findById({ _id: id });
+  if (existData) {
+    const quantity = existData.inventory.quantity;
+    if (quantity > 0 && quantity >= orderQuantity) {
+      const result = await Order.create(orderData);
+      const updatingQuantity = await Product.findByIdAndUpdate(id, {
+        'inventory.quantity': quantity - orderQuantity,
+      });
+      const newExistData: any = await Product.findById({ _id: id });
+      const newQuantity = newExistData.inventory.quantity;
+      console.log(newQuantity);
+      if (newQuantity === 0) {
+        const updatingStock = await Product.findByIdAndUpdate(id, {
+          'inventory.inStock': false,
+        });
+      }
+      return result;
+    }
+    return 'Insufficient quantity available in inventory';
+  }
+  return 'Order not found';
 };
 
 const getProductsFromDB = async (email: any) => {
