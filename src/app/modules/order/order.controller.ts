e92a3d1a338692f06ba3b98e32c3384c8553e2fb
RobinTheRedLight <robinmitra789@gmail.com>
2024-05-22 13:07:41 +0600

    order-controller creation completed

---
+++ src/app/modules/order/order.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { OrderValidationSchema } from './order.validation';
+import { OrderServices } from './order.service';
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const orderData = req.body;
+    const zodParsedData = OrderValidationSchema.parse(orderData);
+
+    const result = await OrderServices.createOrderIntoDB(zodParsedData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Order created successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'something went wrong',
+      error: err,
+    });
+  }
+};
+const getOrders = async (req: Request, res: Response) => {
+  try {
+    let message = '';
+    const email = req.query.email;
+    console.log(email);
+    const result = await OrderServices.getProductsFromDB(email);
+    if (email) {
+      message = 'Orders fetched successfully for user email!';
+    } else {
+      message = 'Orders fetched successfully!';
+    }
+    res.status(200).json({
+      success: true,
+      message: message,
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'something went wrong',
+      error: err,
+    });
+  }
+};
+
+export const OrderControllers = {
+  createOrder,
+  getOrders,
+};
