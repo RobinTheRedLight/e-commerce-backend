ee422c61a70b832297e77cc19c478425d988c0b1
RobinTheRedLight <robinmitra789@gmail.com>
2024-05-22 16:57:19 +0600

    added comments in complex sections

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -4,18 +4,26 @@ import { Order } from './order.model';
 
 const createOrderIntoDB = async (orderData: TOrder) => {
   const id = orderData.productId.toString();
+  // Extract the quantity from the order data.
   const orderQuantity = orderData.quantity;
+  // Check if the product exists in the database.
   const existData = await Product.findById({ _id: id });
   if (existData) {
+    // Get the current inventory quantity.
     const quantity = existData.inventory.quantity;
+    // Check if there is enough inventory to fulfill the order.
     if (quantity > 0 && quantity >= orderQuantity) {
       const result = await Order.create(orderData);
+      // Update the inventory quantity.
       const updatingQuantity = await Product.findByIdAndUpdate(id, {
         'inventory.quantity': quantity - orderQuantity,
       });
+      // Fetch the updated product data.
       const newExistData: any = await Product.findById({ _id: id });
+      // Get the new inventory quantity.
       const newQuantity = newExistData.inventory.quantity;
       console.log(newQuantity);
+      // Update the inStock status if quantity is 0.
       if (newQuantity === 0) {
         const updatingStock = await Product.findByIdAndUpdate(id, {
           'inventory.inStock': false,
